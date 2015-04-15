572e544e3c86bea11b1f82da467a501ec4e93350
Kevin <kevin78515@gmail.com>
2015-04-15 21:07:22 +0800

    Answer

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ var fs = require("fs"),
 fs.readFile("./grades.csv", "utf8", function(err, data){
 	// console.log(data);
 	rows = data.split("\n");
-	fs.writeFileSync(outputPath,"");
-	for(var i=0; i<rows.length; i++){
-		fs.appendFileSync(outputPath,rows[i]);
+	fs.writeFileSync(outputPath,"Student ID,GPA");
+	for(var i=1; i<rows.length; i++){
+		var elems = rows[i].split(','),
+			score = 0;
+		for(var j=1; j<elems.length; j++){
+			if (elems[j]=='A') {
+				score += 4;
+			} else if (elems[j]=='B') {
+				score += 3;
+			} else if (elems[j]=='C') {
+				score += 2;
+			} else if (elems[j]=='D') {
+				score += 1;
+			}
+		}
+		fs.appendFileSync(outputPath,elems[0]+","+(score/5));
 	}
 });
