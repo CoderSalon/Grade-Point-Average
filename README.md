b0d42d1d7619ead0670e706f886528a4dbb7e026
Kevin <kevin78515@gmail.com>
2015-04-19 16:10:01 +0800

    加入補充

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 最常見的成績平均績點系統為四分制：A為4分；B為3分；C為2分；D為1分；F為0分，如果有+號加0.3分（例如B+即3.33分），有-號則減0.3分（例如B-即2.67分）。通常每科最高分為4分，但部份院校會設有A+，即最高分為4.3分。而個別院校的計算方法也可能有少許差異。  
 
-請試著寫出一個幫助學校計算GPA的程式吧！
+請試著寫出一個幫助學校計算GPA的程式吧！  
+期望輸出的結果範例如下：  
+[https://github.com/CoderSalon/Grade-Point-Average/blob/master/gpa_example.csv](https://github.com/CoderSalon/Grade-Point-Average/blob/master/gpa_example.csv)
--- app.js
+++ app.js
@@ -2,10 +2,19 @@ var fs = require("fs"),
 	outputPath="./gpa.csv";
 
 fs.readFile("./grades.csv", "utf8", function(err, data){
-	// console.log(data);
+
+	// 這行程式碼是用來把讀進來的資料一行一行的「切開」，
+	// 成為一個陣列(Array)，放進rows裡面
 	rows = data.split("\n");
+
 	fs.writeFileSync(outputPath,"");
 	for(var i=0; i<rows.length; i++){
+
+		//==== 以下是你要改的地方 ====//
+		
 		fs.appendFileSync(outputPath,rows[i]);
+		
+		//==== 以上是你要改的地方 ====//
+		
 	}
 });
